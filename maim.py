23c910356bde0e74a5d54856f689269870ae7b9b
Rubia Braga <rubiaemilly29@gmail.com>
2025-05-10 23:31:52 -0300

    add task que comentar em doc

--- maim.py
+++ maim.py
@@ -1,18 +1,24 @@
 from langchain_openai import ChatOpenAI
 from browser_use import Agent
 from dotenv import load_dotenv
-load_dotenv()
-
 import asyncio
+
+load_dotenv()
 
 llm = ChatOpenAI(model="gpt-4o")
 
 async def main():
-    agent = Agent(
-        task="acesse o link https://docs.google.com/document/d/1cRQZOB2Nzlf2js09CH3irm073HX1Rs6hMYGzJ-MWB3k/edit?usp=sharing , selecione a aba Arquivo, depois baixar, depois microsoft word",
-        llm=llm,
-    )
+    task = """Acesse o link https://docs.google.com/document/d/1cRQZOB2Nzlf2js09CH3irm073HX1Rs6hMYGzJ-MWB3k/edit?usp=sharing
+    Verifique se é possível adicionar um comentário no documento
+    Adicione o comentário 'Teste' no documento
+    Verifique se o comentário foi adicionado com sucesso
+    """
+    
+    
+    agent = Agent(task=task, llm=llm)
     result = await agent.run()
-    print(result)
 
+
+"""     print(f"Resultado: {result}")
+ """
 asyncio.run(main())
